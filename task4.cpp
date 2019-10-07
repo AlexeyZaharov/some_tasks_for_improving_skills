e33d2c37453c2087f78a28d1882217739a41357a
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2019-10-07 14:01:11 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -5,9 +5,10 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 // Объяснение - сначала комментарии в main, потом в find_sum,
-// потом в find_subsegment с 3 параметрами, затем - с 5 параметрами
+// потом в find_subsegment с 3 параметрами, затем - с 7 параметрами
 
 auto find_subsegment(const std::vector<int>& v, const int& sum, const int& koef) {
     size_t i = 1, j = 0;
@@ -41,52 +42,44 @@ auto find_subsegment(const std::vector<int>& v, const int& sum, const int& koef)
     }
 }
 
-auto find_subsegment(const std::vector<int> &v, size_t begin, size_t end, const int &cur_sum, const int &sum) {
+auto find_subsegment(const std::vector<int> &v, size_t begin, size_t end,
+        std::unordered_map<int, size_t>& left_amounts,
+        std::unordered_map<int, size_t>& right_amounts,
+        const int &left_sum, const int& right_sum) {
 
-    if (begin >= end) return std::pair<size_t, size_t>(0, 0);
+    // так как на прошлом шаге не обнаружилась заданная сумма, то это значит, что ни v[begin], ни v[end - 1]
+    // не принадлежат подотрезку с заданной суммой. С другой стороны, мы к заданной сумме можем добавить значение
+    // v[begin] или v[end - 1], чтобы "расширить окно" и найти заданную сумму. Главное, помнить, сколько раз мы
+    // расширяли это окно, чтобы затем лишние элементы не попали в подотрезок
 
-    if (cur_sum == sum) {
-        return std::pair<size_t, size_t>(begin,end);
+    if (begin >= end) {
+        return std::pair<size_t, size_t>(0, 0);
     }
 
-    // если найденная сумма не равна заданной, пытаемся сузить "окно" сначала
-    // слева направо, потом справа налево
+    // действуем следующим образом: добавиляем не попавший в подотрезок левый элемент к заданной сумме и ищем его
+    // в таблице левых сумм. Если нашли, то наш подинтервал начинается с элемента, следуещего после begin и до значения
+    // таблицы с ключом новой суммы
 
-    int new_sum = cur_sum;
-    for (size_t i = begin; i < end - 1; ++i) {
-        new_sum -= v[i];
-        if (new_sum == sum) {
-            return std::pair<size_t, size_t>(i + 1,end);
-        }
+    if (left_amounts.find(left_sum + v[begin]) != left_amounts.end()) {
+        return std::pair<size_t, size_t>(begin + 1, left_amounts[left_sum + v[begin]] + 1);
     }
 
-    // потом справа налево
+    // Аналогично для правых сумм
 
-    new_sum = cur_sum;
-    for (int i = end - 1; i > begin; --i) {
-        new_sum -= v[i];
-        if (new_sum == sum) {
-            return std::pair<size_t, size_t>(begin,i);
-        }
+    if (right_amounts.find(right_sum + v[end - 1]) != right_amounts.end()) {
+        return std::pair<size_t, size_t>(right_amounts[right_sum + v[end - 1]], end - 1);
     }
 
-    // если заданной суммы не оказалось в проверенных случаях, это значит, что
-    // v[begin] и v[end - 1] не принадлежат подотрезку с заданной суммой (так
-    // как мы проверили все возможные "окна" с данными элементами)
-    // в таком случае пытаемся найти подотрезок в массиве [begin + 1; end - 1]
-
-    new_sum = cur_sum - v[begin] - v[end - 1];
-
-    return find_subsegment(v, begin + 1, end - 1, new_sum, sum);
+    return find_subsegment(v, begin + 1, end - 1, left_amounts, right_amounts,
+            left_sum + v[begin], right_sum + v[end - 1]);
 }
 
 auto find_sum(const std::vector<int>& v, const int& sum) {
     int new_sum = 0, max = v[0], min = v[0];
 
-    // считаем сумму всех чисел массива, находя минимум и максимум
+    // находим минимум и максимум
 
     for (auto& i : v) {
-        new_sum += i;
         max = max > i ? max : i;
         min = min < i ? min : i;
     }
@@ -110,7 +103,7 @@ auto find_sum(const std::vector<int>& v, const int& sum) {
         // иначе нужно искать, двигая "окно" по массиву
         // koef - костыль, чтобы не писать отдельные функции для неотрицательного и
         // неположительного массива: он либо 1, либо -1 (всегда будем искать положительную
-        // сумму для неотрицательную элементов)
+        // сумму для неотрицательных элементов)
         // нужен для того, чтобы понимать, следует ли расширять "окно" или сужать его
 
         int koef = sum / std::abs(sum);
@@ -118,27 +111,52 @@ auto find_sum(const std::vector<int>& v, const int& sum) {
     }
     else {
 
-        // если числа различны по знаку, используем динамическое программирование
+        // если числа различны по знаку, используем две хеш-таблицы: левые суммы и правые суммы
+        // ключами таблицы левых сумм будут суммы от 1 до k элемента, k меняется от 1 до v.size(),
+        // значениями таблицы будут индекс крайнего правого элемента (k);
+        // аналогично для таблицы правых сумм, только суммы будут от k до v.size(), k меняется от 1 до v.size()
 
-        return find_subsegment(v, 0, v.size(), new_sum, sum);
+        std::unordered_map<int, size_t> left_amounts, right_amounts;
+        int left_sum = 0, right_sum = 0;
+
+        for (size_t i = 0; i < v.size(); ++i) {
+            left_sum += v[i];
+            left_amounts[left_sum] = i;
+
+            right_sum += v[v.size() - 1 - i];
+            right_amounts[right_sum] = v.size() - 1 - i;
+        }
+
+        // если среди найденных сумм нашлась заданная сумма, возвращаем значения полуинтервала
+        // [begin_index, end_index) такого, что сумма всех чисел с индексами данного полуинтервала равна заданной сумме
+
+        if (left_amounts.find(sum) != left_amounts.end()) {
+            return std::pair<size_t, size_t>(0, left_amounts[sum] + 1);
+        }
+
+        if (right_amounts.find(sum) != right_amounts.end()) {
+            return std::pair<size_t, size_t>(right_amounts[sum], v.size());
+        }
+
+        // если среди данных таблиц суммы не нашлось, ищем дальше
+
+        return find_subsegment(v, 0, v.size(), left_amounts, right_amounts, sum, sum);
     }
 }
 
 int main() {
     std::vector<int> v = {-9,0,3,100,-7,5,6};
 
-    int sum = 96;
+    int sum = 98;
 
     // Сложность алгоритма в случае "монотонного" массива (все числа имеют один и тот же
     // знак либо нулевые) - O(n), достаточно двух проходов по массиву: одного для поиска минимума
-    // и максимума, второго - для поиска заданной суммы;
-    // Если массив не монотонный - O(n^2), так как придется дважды проходить по каждому "окну" размером
-    // n, n-2, n-4, ...
-    // В обоих случаях сложность по памяти - O(1)
+    // и максимума, второго - для поиска заданной суммы; сложность по памяти - O(1);
+    // Если массив не монотонный - O(n), но придется использовать O(n) памяти
 
     auto pair = find_sum(v, sum);
-    
-    if (piar.first == pair.second) {
+
+    if (pair.first == pair.second) {
         std:: cout << "It is impossible";
     }
     else {
